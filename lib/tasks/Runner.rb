a63d7c62af225c0ba55df3c7c1712fb0b56e767d
Fritz <fritz@theshizit.com>
2012-12-06 10:55:44 +0100

    feature #13

--- lib/tasks/Runner.rb
+++ lib/tasks/Runner.rb
@@ -16,7 +16,7 @@ class Runner
     
     if system('ffmpeg -i "%s" -f wav "%s/tmp/waveform/%s.wav"' % [path, Rails.root.to_s, filename])
       ActiveRecord::Base.logger.info("Converting mp3 to wav complete.")
-      if system('waveform -W600 -H100 %s/tmp/waveform/%s.wav %s/tmp/waveform/%s.png' % [Rails.root.to_s, filename, Rails.root.to_s, filename])
+      if system('waveform -W800 -H50 %s/tmp/waveform/%s.wav %s/tmp/waveform/%s.png' % [Rails.root.to_s, filename, Rails.root.to_s, filename])
         ActiveRecord::Base.logger.info("Generating waveform complete.")
         audio.wave = File.open("%s/tmp/waveform/%s.png" % [Rails.root.to_s, filename]);
         audio.save
